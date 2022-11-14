061a64ccacc814ca3650daa102054195ea9a35d8
Lucas Gabriel dos Santos <110554177+luzka1@users.noreply.github.com>
2022-11-14 19:39:59 -0300

    Add files via upload

---
+++ VisiArte/src/App.js
@@ -0,0 +1,19 @@
+
+import './css/App.css';
+import Header from './components/Header';
+import Entrada from './components/Entrada';
+import HeaderRede from './components/HeaderRede';
+
+function App() {
+  return (
+    <>
+      <body>
+      <Header />
+      <Entrada />
+      </body>
+
+    </>
+  );
+}
+
+export default App;
---
+++ VisiArte/src/components/Entrada.jsx
@@ -0,0 +1,22 @@
+import styles from '../css/Entrada.module.css';
+
+
+function Entrada() {
+    return (
+        <>
+            <section className={styles.container}>
+                <div className={styles.container2}>
+                    <div className={styles.textBox}>
+
+                        <h1>VisiArte</h1>
+                        <h3>Slogan é o caralho! Senta no meu piipi! </h3>
+                    </div>
+                </div>
+                <div className={styles.aa}></div>
+            </section>
+        </>
+    )
+}
+
+export default Entrada;
+
