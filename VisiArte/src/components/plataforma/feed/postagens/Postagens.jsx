99001b91b450a55dad60e37fabc311f94ab45c4e
gabrielalmeidasantos <gabriel.almeiidda@gmail.com>
2022-11-16 22:25:14 -0300

    feed finalizado

--- VisiArte/src/components/plataforma/feed/postagens/Postagens.jsx
+++ VisiArte/src/components/plataforma/feed/postagens/Postagens.jsx
@@ -2,10 +2,27 @@ import React from "react";
 import Postagem from "./postagem/Postagem";
 import styles from "./Postagens.module.css";
 
+import perfil from "../../../../img/foto-perfil.png";
+import fundo_perfil from "../../../../img/fundo_postagem.png";
+
 function Postagens() {
     return (
         <div className={styles.postagens}>
-            <Postagem />
+            <Postagem
+                img_perfil={perfil}
+                nome="#username"
+                img_postagem={fundo_perfil}
+            />
+            <Postagem
+                img_perfil={perfil}
+                nome="#username"
+                img_postagem={fundo_perfil}
+            />
+            <Postagem
+                img_perfil={perfil}
+                nome="#username"
+                img_postagem={fundo_perfil}
+            />
         </div>
     );
 }
--- VisiArte/src/components/plataforma/feed/postagens/postagem/Postagem.jsx
+++ VisiArte/src/components/plataforma/feed/postagens/postagem/Postagem.jsx
@@ -1,19 +1,29 @@
 import React from "react";
 import styles from "./Postagem.module.css";
 
-import perfil from "../../../../../img/foto-perfil.png";
+import curtir from "../../../../../img/curtir.png";
+import comentar from "../../../../../img/comentar.png";
 
-function Postagem() {
+function Postagem({ img_perfil, nome, img_postagem }) {
     return (
         <div className={styles.postagem}>
             <div className={styles.perfil}>
-                <img src={perfil} alt="" />
+                <img src={img_perfil} alt="" />
+                <span>{nome}</span>
             </div>
-            <p></p>
-            <img src="" alt="" />
-            <div></div>
-            <form>
-                <input type="text" placeholder="" />
+            <p className={styles.descricao}>Texto sobre a postagem</p>
+            <img src={img_postagem} alt="" className={styles.foto_postagem} />
+            <div className={styles.interagir}>
+                <img src={curtir} alt="" className={styles.curtir} />
+                <img src={comentar} alt="" className={styles.comentar} />
+            </div>
+            <form className={styles.digitar_comentario}>
+                <input
+                    type="text"
+                    placeholder="Digite um comentário..."
+                    required
+                />
+                <button type="submit">Publicar</button>
             </form>
         </div>
     );
