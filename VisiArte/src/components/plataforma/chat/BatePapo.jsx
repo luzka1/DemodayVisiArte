d151b4d2273fba86bfc0f92348a25f664595ce6a
Kae Lucas <kaeteodoro@gmail.com>
2022-11-28 18:42:59 -0300

    Atualizando chat

--- VisiArte/src/components/plataforma/chat/BatePapo.jsx
+++ VisiArte/src/components/plataforma/chat/BatePapo.jsx
@@ -14,7 +14,12 @@ function BatePapo() {
           <input type="text" id={styles.textarea} />
           {/* <section id={styles.textarea}></section> */}
           <section id={styles.send} >
-          <img src="https://e7.pngegg.com/pngimages/841/271/png-clipart-computer-icons-send-miscellaneous-angle.png" alt="" />
+            <div className={styles.poligon}>
+              <svg width="37" height="39" viewBox="0 0 795 918" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M794.625 459.125L0.37496 917.685L245.625 459.125L0.375 0.564418L794.625 459.125Z" fill="#F95800" />
+              </svg>
+
+            </div>
           </section>
 
         </section>
