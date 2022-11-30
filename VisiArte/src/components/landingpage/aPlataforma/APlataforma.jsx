c1aa98f8687674d7ea41dc7837eb986473053db8
Kae Lucas <kaeteodoro@gmail.com>
2022-11-30 08:19:59 -0300

    Alterando o chat e comentários

--- VisiArte/src/components/landingpage/aPlataforma/APlataforma.jsx
+++ VisiArte/src/components/landingpage/aPlataforma/APlataforma.jsx
@@ -14,6 +14,7 @@ function APlataforma() {
                             Venha participar da rede social que foi desenvolvida para <text>todo tipo de Arte e Artista</text>. <br />
                             <p> Temos a missão de ser uma plataforma simples e intuitiva para você artista que quer <text>espalhar conhecimento</text>,
                             ou você que quer <text>aprender</text> novas técnicas e conceitos de arte.{" "}</p>
+                            
                         </p>
                     </div>
                     <div className={styles.images}>
