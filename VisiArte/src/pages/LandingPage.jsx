80bc05d9e711cc6f5a39a7b6903250a80a7ef226
gabrielalmeidasantos <gabriel.almeiidda@gmail.com>
2022-12-03 18:56:19 -0300

    ajustando header

--- VisiArte/src/pages/LandingPage.jsx
+++ VisiArte/src/pages/LandingPage.jsx
@@ -9,26 +9,15 @@ import APlataforma from "../components/landingpage/aPlataforma/APlataforma";
 import Comentarios from "../components/landingpage/comentarios/Comentarios";
 import Footer from "../components/landingpage/footer/Footer";
 import { motion, useInView } from "framer-motion";
-<<<<<<< HEAD
 import { useRef } from "react";
-import Vlibras from "../components/Vlibras/Vlibras"
-=======
-import { useRef} from "react";
->>>>>>> fc60c7b508838115b2d8dc324634b018afb97df3
 
 function LandingPage() {
-
     const ref = useRef(null);
 
     const isInView = useInView(ref, { once: true });
 
     return (
-<<<<<<< HEAD
         <div className="ContainerLandingPage">
-            <Vlibras />
-=======
-        <div className="ContainerLandingPage">          
->>>>>>> fc60c7b508838115b2d8dc324634b018afb97df3
             <Header />
             <Entrada />
             <SliderPrin />
@@ -40,11 +29,13 @@ function LandingPage() {
                 style={{
                     transform: isInView ? "none" : "translateY(10vh)",
                     opacity: isInView ? 1 : 0,
-                    transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
-                }}>
+                    transition:
+                        "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
+                }}
+            >
                 <TodaParte />
             </motion.div>
-            
+
             <APlataforma />
             <Comentarios />
             <Footer />
