b38d5b276a39a9e2ce82c0f49d8e9b73ddc019ba
gabrielalmeidasantos <gabriel.almeiidda@gmail.com>
2022-12-12 01:08:52 -0300

    preparando pro login

--- VisiArte/src/components/Cadastro_Login/BtnCadastroLogin/BtnCadastroLogin.jsx
+++ VisiArte/src/components/Cadastro_Login/BtnCadastroLogin/BtnCadastroLogin.jsx
@@ -1,13 +1,16 @@
-import { Link } from "react-router-dom"
-import Styles from "./BtnCadastroLogin.module.css"
+import { Link } from "react-router-dom";
+import Styles from "./BtnCadastroLogin.module.css";
 
-function BtnCadastroLogin(props){
-    return(
-        <Link to="/feed" className={`${Styles.Acessar} ${props.className}`} onClick={props.onClick}>
+function BtnCadastroLogin(props) {
+    return (
+        <div
+            className={`${Styles.Acessar} ${props.className}`}
+            onClick={props.onClick}
+        >
             <p>{props.Acessar}</p>
             <span>&#x276F;</span>
-        </Link>
-    )
-};
+        </div>
+    );
+}
 
 export default BtnCadastroLogin;
--- VisiArte/src/components/Cadastro_Login/TelaLogin/TelaDeLogin.jsx
+++ VisiArte/src/components/Cadastro_Login/TelaLogin/TelaDeLogin.jsx
@@ -1,32 +1,91 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { URL_API } from "../../../API";
 import BtnCadastroLogin from "../BtnCadastroLogin/BtnCadastroLogin";
-import Escreva from "../Escreva/Escreva";
 import Styles from "./TelaDeLogin.module.css";
 
+function TelaDeLogin() {
+    const [usuario, setUsuario] = React.useState("");
+    const [senha, setSenha] = React.useState("");
 
-function TelaDeLogin(props){
-    return(
+    const data = {
+        usuarioLogin: usuario,
+        senha: senha,
+    };
+
+    function fazerLogin(e) {
+        e.preventDefault();
+        axios
+            .post(`${URL_API}/usuarios/login`, JSON.stringify(data))
+            .then((response) => {
+                console.log(response.data);
+            });
+    }
+
+    return (
         <>
-            <div className={Styles.DivMeio}>
-                <div className={Styles.MeioBaixo}>
-                    <h1 className={Styles.TextoIniciar}>Conectar-se</h1>
-                    <Escreva type ="TEXT" placeholder ="E-MAIL OU USUARIO"/>
-                    <Escreva type ="password" placeholder ="SENHA"/>
+            <form onSubmit={(e) => fazerLogin(e)}>
+                <div className={Styles.DivMeio}>
+                    <div className={Styles.MeioBaixo}>
+                        <h1 className={Styles.TextoIniciar}>Conectar-se</h1>
+                        <div className={Styles.Iniciando}>
+                            <input
+                                value={usuario}
+                                onChange={({ target }) =>
+                                    setUsuario(target.value)
+                                }
+                                required
+                                type="text"
+                                placeholder="E-MAIL OU USUARIO"
+                                className={Styles.Nome}
+                            />
+                        </div>
 
-                    <div className={Styles.Manter}>
-                        <input type="checkbox" name="ManterLogin" id="ManterLogin" className={Styles.Login} />
-                        <label for="ManterLogin">Manter conectado</label>
+                        <div className={Styles.Iniciando}>
+                            <input
+                                value={senha}
+                                onChange={({ target }) =>
+                                    setSenha(target.value)
+                                }
+                                required
+                                type="password"
+                                placeholder="SENHA"
+                                className={Styles.Nome}
+                            />
+                        </div>
+
+                        <div className={Styles.Manter}>
+                            <input
+                                type="checkbox"
+                                name="ManterLogin"
+                                id="ManterLogin"
+                                className={Styles.Login}
+                            />
+                            <label htmlFor="ManterLogin">
+                                Manter conectado
+                            </label>
+                        </div>
+
+                        <button type="submit">
+                            <BtnCadastroLogin
+                                // Acessar={Acessar}
+                                className={Styles.BtnLogin}
+                            />
+                        </button>
+
+                        {/* <Link to="/" className={Styles.EsqSenha}>
+                            Esqueceu a senha?
+                        </Link> */}
+
+                        <Link to="/cadastro" className={Styles.CrieConta}>
+                            Criar conta
+                        </Link>
                     </div>
-
-                    <BtnCadastroLogin Acessar = {props.Acessar} className={Styles.BtnLogin}/>
-        
-                    <Link to="/" className={Styles.EsqSenha}>Esqueceu a senha?</Link>
-        
-                    <Link to="/cadastro" className={Styles.CrieConta}>Criar conta</Link>
                 </div>
-            </div>
+            </form>
         </>
-    )
+    );
 }
 
 export default TelaDeLogin;
