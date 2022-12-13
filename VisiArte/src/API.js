704173d9661babef027fcd3ad806d3ab28ad15bb
gabrielalmeidasantos <gabriel.almeiidda@gmail.com>
2022-12-12 22:40:19 -0300

    permitindo login

--- VisiArte/src/API.js
+++ VisiArte/src/API.js
@@ -1,23 +1,21 @@
 export const URL_API = "http://localhost:5000/api";
-export const URL_SITE = "http://localhost:3000";
+export const URL_SITE = "http://demoday-visi-arte.vercel.app/";
 
-export function verificaLogin(){
-    let usuario = localStorage.getItem("usuarioLogado");
-    if (usuario) {
-        return true;
-    }
-    return false
+export function verificaLogin() {
+    // let usuario = localStorage.getItem("usuarioLogado");
+    // if (usuario) {
+    //     return true;
+    // }
+    // return false
 }
 
-
-export function logout(){
-    try {
-        localStorage.removeItem("usuarioLogado");
-        localStorage.clear();
-        window.location.reload(); 
-        return true;
-    } catch (error) {
-        return false;
-    }
-
+export function logout() {
+    // try {
+    //     localStorage.removeItem("usuarioLogado");
+    //     localStorage.clear();
+    //     window.location.reload();
+    //     return true;
+    // } catch (error) {
+    //     return false;
+    // }
 }
